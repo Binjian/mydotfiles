8db8f98ed361e452b4995194393fe52403ced745
binjian <binjian.xin@hotmail.com>
2026-06-01 09:58:18 +0800

    [update] nvim plugins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,9 +9,27 @@ return {
         end,
         desc = "Find Plugin File",
       },
+      -- Find files including hidden
+      { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files (hidden)" },
+      -- Live grep including hidden
+      { "<leader>sg", "<cmd>Telescope live_grep hidden=true<cr>", desc = "Live Grep (hidden)" },
     },
     opts = {
       defaults = {
+        -- Example: ignore .git folder but show other hidden files
+        vimgrep_arguments = {
+          "rg",
+          "--hidden",
+          "--glob",
+          "!**/.git/*",
+          -- ... other default args
+        },
+
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         layout_strategy = "horizontal",
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
