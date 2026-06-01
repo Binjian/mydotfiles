cff4e3973110b3ad1d0603d0e4bb408c51ecf9ab
binjian <binjian.xin@hotmail.com>
2026-06-01 09:59:56 +0800

    [add] nvim plugins luarocks, tiktoken

---
+++ .config/nvim/lua/plugins/luarocks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "camspiers/luarocks",
+    opts = {
+      rocks = { "dkjson" },
+      -- The path to the LuaRocks executable. If not set, it will be searched in the system PATH.
+      -- luarocks_cmd = "luarocks",
+      -- The path to the LuaRocks configuration file. If not set, it will use the default configuration.
+      -- config_file = nil,
+      -- The path to the LuaRocks tree where packages are installed. If not set, it will use the default tree.
+      -- rocks_tree = nil,
+    },
+  },
+}
