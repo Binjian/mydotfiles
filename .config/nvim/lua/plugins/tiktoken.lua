dc0a309b02f5f2e3bd3b687289cfef4ce97cee0f
binjian <binjian.xin@hotmail.com>
2026-08-19 11:19:19 +0800

    fix(nvim): update LazyVim configuration

--- .config/nvim/lua/plugins/tiktoken.lua
+++ .config/nvim/lua/plugins/tiktoken.lua
@@ -2,7 +2,13 @@ return {
   {
     "vhyrro/luarocks.nvim",
     priority = 1000,
-    config = true,
+    config = function(_, opts)
+      -- LuaRocks 3.13 keeps dkjson in its vendor directory, but
+      -- luarocks.nvim does not add that directory to package.path.
+      local rocks = vim.fn.stdpath("data") .. "/lazy/luarocks.nvim/.rocks"
+      package.path = package.path .. ";" .. rocks .. "/share/lua/5.1/luarocks/vendor/?.lua"
+      require("luarocks-nvim").setup(opts)
+    end,
     opts = {
       rocks = { "tiktoken_core" },
     },
